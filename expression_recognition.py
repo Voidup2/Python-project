1241709bd45644728babb0d0eabaea8d9661f6cf
Utsav Prakash <126964544+Voidup2@users.noreply.github.com>
2025-02-17 23:21:30 +0530

    Update expression_recognition.py

--- expression_recognition.py
+++ expression_recognition.py
@@ -3,9 +3,8 @@ from deepface import DeepFace
 def recognize_expression(frame):
     result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
     
-    # DeepFace returns a list when multiple faces are detected
     if isinstance(result, list):
-        result = result[0]  # Pick the first detected face
+        result = result[0] 
     
     return result.get('emotion', 'unknown')
 
